b8b45a9de9e5ae5c89f7ac69f0c4b897eecad998
so yeon <alicesysy@swu.ac.kr>
2022-07-18 06:41:42 +0900

    [#3] 넷플릭스 UI 구현

--- Assignment-001/Assignment-001/ViewController.swift
+++ Assignment-001/Assignment-001/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - UI Property
+    
+    @IBOutlet weak var firstMovieImageView: UIImageView!
+    @IBOutlet weak var secondMovieImageView: UIImageView!
+    @IBOutlet weak var thirdMovieImageView: UIImageView!
+    
+    @IBOutlet var movieImageCollection: [UIImageView]!
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setImageView()
     }
-
-
+    
+    private func setImageView() {
+        for image in movieImageCollection {
+            image.layer.cornerRadius = image.layer.frame.width / 2
+            
+            image.layer.borderColor = UIColor.lightGray.cgColor
+            image.layer.borderWidth = 2
+        }
+    }
 }
 
