26f670c82561d947b804568012025bc77b136be2
so yeon <alicesysy@swu.ac.kr>
2022-07-19 12:49:42 +0900

    [#6] 0719 수업

---
+++ TrendMedia/TrendMedia/TrendMedia/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  TrendMedia
+//
+//  Created by 소연 on 2022/07/19.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setBackgroundColor()
+    }
+}
