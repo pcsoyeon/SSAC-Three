f333a7815c51de1f4d3b3998dcc163df8eabb77b
so yeon <alicesysy@swu.ac.kr>
2022-07-20 09:13:26 +0900

    [#7] Trend Media Search UI 구현

---
+++ TrendMedia/TrendMedia/TrendMedia/TrendMediaTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TrendMediaTableViewCell.swift
+//  TrendMedia
+//
+//  Created by 소연 on 2022/07/20.
+//
+
+import UIKit
+
+class TrendMediaTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var movieDateLabel: UILabel!
+    @IBOutlet weak var movieContentLabel: UILabel!
+}
