987b15bde39dc58ba69f08280995d75d74f751d2
so yeon <alicesysy@swu.ac.kr>
2022-07-28 09:21:21 +0900

    [#18] 프로젝트 폴더링

---
+++ NetworkBasic/NetworkBasic/Source/Protocol/ViewPresentableProtocol.swift
@@ -0,0 +1,18 @@
+//
+//  ViewPresentableProtocol.swift
+//  NetworkBasic
+//
+//  Created by 소연 on 2022/07/28.
+//
+
+import Foundation
+
+/*
+ ----Protocol
+ ----Delegate
+ */
+
+protocol ViewPresentableProtocol {
+    func configureView()
+    func configureLabel()
+}
