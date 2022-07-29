380d54a8ae81f5bdb0f7f02d89576431d5973ec3
so yeon <alicesysy@swu.ac.kr>
2022-07-29 09:14:47 +0900

    [#19] Location 파일 추가

---
+++ NetworkBasic/NetworkBasic/Source/Screen/Location/Controller/LocationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LocationViewController.swift
+//  NetworkBasic
+//
+//  Created by 소연 on 2022/07/29.
+//
+
+import UIKit
+
+class LocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
