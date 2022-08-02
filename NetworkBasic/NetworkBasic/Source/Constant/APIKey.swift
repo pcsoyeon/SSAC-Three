37624e89dca5f2ede493740ee1ca43f30a22948e
so yeon <alicesysy@swu.ac.kr>
2022-08-02 10:18:38 +0900

    [#23] 파일 이동 및 이름 변경

---
+++ NetworkBasic/NetworkBasic/Source/Constant/APIKey.swift
@@ -0,0 +1,8 @@
+//
+//  APIKey.swift
+//  NetworkBasic
+//
+//  Created by 소연 on 2022/08/02.
+//
+
+import Foundation
---
+++ NetworkBasic/NetworkBasic/Source/Screen/Network/Model/BeerResponse.swift
@@ -0,0 +1,8 @@
+//
+//  BeerDataModel.swift
+//  NetworkBasic
+//
+//  Created by 소연 on 2022/08/02.
+//
+
+import Foundation
