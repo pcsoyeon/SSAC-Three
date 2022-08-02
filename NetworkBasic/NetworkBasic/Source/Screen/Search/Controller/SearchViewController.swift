65aec578be581e64a5a38418c2807893f33cd7de
so yeon <alicesysy@swu.ac.kr>
2022-08-02 10:20:18 +0900

    [#23] 영화 GET API 연결

--- NetworkBasic/NetworkBasic/Source/Screen/Search/Controller/SearchViewController.swift
+++ NetworkBasic/NetworkBasic/Source/Screen/Search/Controller/SearchViewController.swift
@@ -7,27 +7,43 @@
 
 import UIKit
 
+import Alamofire
+import SwiftyJSON
+
 class SearchViewController: UIViewController {
     
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var searchTableView: UITableView!
     
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setTableView()
+        configureTableView()
+        configureSearchBar()
+        requestBoxOffice(date: "20220801")
     }
     
     // MARK: - Custom Method
     
-    private func setTableView() {
+    private func configureTableView() {
         searchTableView.delegate = self
         searchTableView.dataSource = self
+        
+        searchTableView.backgroundColor = .clear
+        searchTableView.separatorColor = .clear
+        searchTableView.rowHeight = 60
         
         let nib = UINib(nibName: SearchTableViewCell.identifier, bundle: nil)
         searchTableView.register(nib, forCellReuseIdentifier: SearchTableViewCell.identifier)
     }
+    
+    private func configureSearchBar() {
+        searchBar.delegate = self
+    }
 }
+
+// MARK: - UITableView Protocol
 
 extension SearchViewController: UITableViewDelegate {
     
@@ -35,11 +51,40 @@ extension SearchViewController: UITableViewDelegate {
 
 extension SearchViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 50
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: SearchTableViewCell.identifier, for: indexPath) as? SearchTableViewCell else { return UITableViewCell() }
         return cell
+    }
+}
+
+// MARK: - UISearchBar
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if let date = searchBar.text {
+            requestBoxOffice(date: date)
+        }
+    }
+}
+
+// MARK: - Network
+
+extension SearchViewController {
+    private func requestBoxOffice(date: String) {
+        let url = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=9c2f46329f6dc7e3e31aba631ca48c49&targetDt=\(date)"
+        
+        AF.request(url, method: . get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                dump(json)
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
