e17a54956f58791e19c48826eb355ed0c49986c7
so yeon <alicesysy@swu.ac.kr>
2022-08-03 18:54:33 +0900

    [#24] 프로젝트 폴더링

---
+++ TMDBProject/TMDBProject/Network/Base/URLConstant.swift
@@ -0,0 +1,13 @@
+//
+//  URLConstant.swift
+//  TMDBProject
+//
+//  Created by 소연 on 2022/08/03.
+//
+
+import Foundation
+
+struct URLConstant {
+    static let baseURL = "https://api.themoviedb.org/3"
+    static let trendingURL = "/trending/all/day"
+}
---
+++ TMDBProject/TMDBProject/Screen/Trend/Controller/TrendViewController.swift
@@ -0,0 +1,27 @@
+//
+//  TrendViewController.swift
+//  TMDBProject
+//
+//  Created by 소연 on 2022/08/03.
+//
+
+import UIKit
+
+
+
+class TrendViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchTrendMedia()
+    }
+    
+}
+
+// MARK: - Network
+
+extension TrendViewController {
+    private func fetchTrendMedia() {
+        
+    }
+}
