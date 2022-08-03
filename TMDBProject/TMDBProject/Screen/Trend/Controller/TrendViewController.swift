73e5e9f8b6374ed8d9bd901001a94e1087510aea
so yeon <alicesysy@swu.ac.kr>
2022-08-03 23:21:28 +0900

    [#24] 트렌드 미디어 GET API 연결

--- TMDBProject/TMDBProject/Screen/Trend/Controller/TrendViewController.swift
+++ TMDBProject/TMDBProject/Screen/Trend/Controller/TrendViewController.swift
@@ -7,21 +7,142 @@
 
 import UIKit
 
+import Alamofire
+import SwiftyJSON
 
+enum MediaType: String, CaseIterable {
+    case all
+    case movie
+    case tv
+    case person
+}
 
-class TrendViewController: UIViewController {
+enum TimeType: String, CaseIterable {
+    case day
+    case week
+}
 
+final class TrendViewController: UIViewController {
+
+    @IBOutlet weak var mediaCollectionView: UICollectionView!
+    
+    // MARK: - Property
+    
+    let mediaType = MediaType.movie.rawValue
+    let timeType = TimeType.day.rawValue
+    
+    private var trendList: [TrendData] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        fetchTrendMedia()
+        configureCollectionView()
+        fetchTrendMedia(type: mediaType, time: timeType)
     }
     
+    private func configureCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        let spacing: CGFloat = 5
+        let width = UIScreen.main.bounds.width - (spacing * 2)
+        layout.itemSize = CGSize(width: width, height: (width * 1.2))
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        mediaCollectionView.collectionViewLayout = layout
+        
+        mediaCollectionView.dataSource = self
+        
+        mediaCollectionView.register(UINib(nibName: TrendCollectionViewCell.reuseIdentifier, bundle: nil), forCellWithReuseIdentifier: TrendCollectionViewCell.reuseIdentifier)
+    }
+}
+
+// MARK: - UICollectionView Protocol
+
+extension TrendViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return trendList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TrendCollectionViewCell.reuseIdentifier, for: indexPath) as? TrendCollectionViewCell else { return UICollectionViewCell() }
+        cell.setData(trendList[indexPath.item])
+        return cell
+    }
 }
 
 // MARK: - Network
 
 extension TrendViewController {
-    private func fetchTrendMedia() {
+    private func fetchTrendMedia(type: String, time: String) {
+        let url = URLConstant.baseURL + URLConstant.trendingURL + "?api_key=\(APIKey.APIKey)"
         
+        let params: Parameters = ["media_type" : type,
+                                  "time_window" : time]
+        
+        AF.request(url, method: .get, parameters: params).validate(statusCode: 200...500).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("============== 전체 데이터 ===============")
+                print(json)
+                
+                let statusCode = response.response?.statusCode ?? 500
+                if statusCode == 200 {
+                    let page = json["page"].intValue
+                    let totalPage = json["total_pages"].intValue
+                    
+                    for media in json["results"].arrayValue {
+                        let video = media["video"].boolValue
+                        
+                        let backDrop = media["backdrop_path"].stringValue
+                        let poster = media["poster_path"].stringValue
+                        
+                        let voteCount = media["vote_count"].intValue
+                        
+                        let mediaType = media["media_type"].stringValue
+                        
+                        let originalTitle = media["original_title"].stringValue
+                        let title = media["title"].stringValue
+                        
+                        let id = media["id"].intValue
+                        
+                        let releaseDate = media["release_date"].stringValue
+                        
+                        let voteAverage = media["vote_average"].doubleValue
+                        
+                        let adult = media["adult"].boolValue
+                        
+                        let overview = media["overview"].stringValue
+                        
+                        let popularity = media["popularity"].doubleValue
+                        
+                        let trendData = TrendData(video: video,
+                                                  backdropPath: backDrop,
+                                                  posterPath: poster,
+                                                  voteCount: voteCount,
+                                                  mediaType: mediaType,
+                                                  originalTitle: originalTitle,
+                                                  title: title,
+                                                  id: id,
+                                                  releaseDate: releaseDate,
+                                                  voteAverage: voteAverage,
+                                                  adult: adult,
+                                                  overview: overview,
+                                                  popularity: popularity)
+                        
+                        self.trendList.append(trendData)
+                        self.mediaCollectionView.reloadData()
+                    }
+                    
+                    print("============== 사용할 데이터 ===============")
+                    print(self.trendList)
+                } else {
+                    
+                }
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
