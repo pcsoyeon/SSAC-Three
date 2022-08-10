9bee42acf91278c24382a9d06bbd1ad96a35d12d
so yeon <alicesysy@swu.ac.kr>
2022-08-10 19:37:52 +0900

    [#29] Swift 문법 - 일급객체/클로저

--- FirstClassObject.playground/Contents.swift
+++ FirstClassObject.playground/Contents.swift
@@ -319,5 +319,27 @@ for score in examScore {
 print(totalCount / 8)
 
 let totalCountUsingReduce = examScore.reduce(0) { return $0 + $1 }
-print(totalCountUsingReduce / 8
-)
+print(totalCountUsingReduce / 8)
+
+
+// 후행 클로저
+func calculate(a: Int, b: Int, method: (Int, Int) -> Int) -> Int {
+    return method(a, b)
+}
+
+var result: Int
+result = calculate(a: 10, b: 10, method: { (left: Int, right: Int) in
+    return left + right
+})
+
+result = calculate(a: 12, b: 12) { (left: Int, right: Int) -> Int in
+    return left + right
+}
+
+result = calculate(a: 12, b: 12) { (left: Int, right: Int) in
+    return left + right
+}
+
+result = calculate(a: 12, b: 12) {
+    return $0 + $1
+}
