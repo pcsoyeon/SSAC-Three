179ffde25f41b6660d4b37874eecb1ae9e53cf11
so yeon <alicesysy@swu.ac.kr>
2022-08-12 00:38:05 +0900

    [#30] 내 위치, 전체 영화관 위치 UI 구현

--- TMDBProject/TMDBProject/Screen/MyMedia/Controller/MapViewController.swift
+++ TMDBProject/TMDBProject/Screen/MyMedia/Controller/MapViewController.swift
@@ -17,58 +17,84 @@ class MapViewController: UIViewController {
     
     // MARK: - Property
     
-    private var locationManager = CLLocationManager()
-    private let center = CLLocationCoordinate2D(latitude: 37.517829, longitude: 126.886270)
+    private let locationManager = CLLocationManager()
+    private var annotation = MKPointAnnotation()
+    
+    private var theaterList = TheaterList().mapAnnotations
     
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setRegionAndAnnotation(center: center)
+        locationManager.delegate = self
+        
+        setRegionAndAnnotation(title: "청년취업사관학교 영등포 캠퍼스")
     }
     
     // MARK: - Custom Method
     
-    private func setRegionAndAnnotation(center: CLLocationCoordinate2D) {
-        let region = MKCoordinateRegion(center: center, latitudinalMeters: 800, longitudinalMeters: 800)
-        mapView.setRegion(region, animated: true)
+    private func setRegionAndAnnotation(center: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 37.517829, longitude: 126.886270),
+                                        title: String) {
+        let region = MKCoordinateRegion(center: center, latitudinalMeters: 1200, longitudinalMeters: 1200)
         
-        let annotation = MKPointAnnotation()
+        mapView.setRegion(region, animated: true)
+        setAnnotation(center: center, title: title)
+        
+        for theater in theaterList {
+            let center = CLLocationCoordinate2D(latitude: theater.latitude, longitude: theater.longitude)
+            self.setAnnotation(center: center, title: theater.location)
+        }
+    }
+    
+    func setAnnotation(center: CLLocationCoordinate2D, title: String) {
+        let annotation =  MKPointAnnotation()
+        
         annotation.coordinate = center
-        annotation.title = "SeSAC 영등포 캠퍼스"
+        annotation.title = title
+        
         mapView.addAnnotation(annotation)
     }
     
+    // MARK: - IBAction
+    
     @IBAction func touchUpFilterButton(_ sender: UIBarButtonItem) {
-        let optionMenu = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
         
-        let megaBoxAction = UIAlertAction(title: "메가박스", style: .default, handler: {
-            (alert: UIAlertAction!) -> Void in
-        })
+        actionSheet.addAction(UIAlertAction(title: "메가박스", style: .default, handler: {_ in
+            self.mapView.removeAnnotations(self.mapView.annotations)
+            for cinema in self.theaterList.filter({$0.type == "메가박스"}) {
+                let center = CLLocationCoordinate2D(latitude: cinema.latitude, longitude: cinema.longitude)
+                self.setAnnotation(center: center, title: cinema.location)
+            }
+        }))
         
-        let lotteAction = UIAlertAction(title: "롯데시네마", style: .default, handler: {
-            (alert: UIAlertAction!) -> Void in
-        })
+        actionSheet.addAction(UIAlertAction(title: "롯데시네마", style: .default, handler: {_ in
+            self.mapView.removeAnnotations(self.mapView.annotations)
+            for cinema in self.theaterList.filter({$0.type == "롯데시네마"}) {
+                let center = CLLocationCoordinate2D(latitude: cinema.latitude, longitude: cinema.longitude)
+                self.setAnnotation(center: center, title: cinema.location)
+            }
+        }))
         
-        let cgvAction = UIAlertAction(title: "CGV", style: .default, handler: {
-            (alert: UIAlertAction!) -> Void in
-        })
+        actionSheet.addAction(UIAlertAction(title: "CGV", style: .default, handler: {_ in
+            self.mapView.removeAnnotations(self.mapView.annotations)
+            for cinema in self.theaterList.filter({$0.type == "CGV"}) {
+                let center = CLLocationCoordinate2D(latitude: cinema.latitude, longitude: cinema.longitude)
+                self.setAnnotation(center: center, title: cinema.location)
+            }
+        }))
         
-        let totalAction = UIAlertAction(title: "전체보기", style: .default, handler: {
-            (alert: UIAlertAction!) -> Void in
-        })
+        actionSheet.addAction(UIAlertAction(title: "전체보기", style: .default, handler: {_ in
+            self.mapView.removeAnnotations(self.mapView.annotations)
+            for cinema in self.theaterList {
+                let center = CLLocationCoordinate2D(latitude: cinema.latitude, longitude: cinema.longitude)
+                self.setAnnotation(center: center, title: cinema.location)
+            }
+        }))
         
-        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: {
-            (alert: UIAlertAction!) -> Void in
-        })
+        actionSheet.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
         
-        optionMenu.addAction(megaBoxAction)
-        optionMenu.addAction(lotteAction)
-        optionMenu.addAction(cgvAction)
-        optionMenu.addAction(totalAction)
-        optionMenu.addAction(cancelAction)
-        
-        self.present(optionMenu, animated: true, completion: nil)
+        self.present(actionSheet, animated: true)
     }
 }
 
@@ -77,3 +103,81 @@ class MapViewController: UIViewController {
 extension MapViewController: MKMapViewDelegate {
     
 }
+
+// MARK: - Authorization Method
+
+extension MapViewController {
+    func checkUserDeviceLocationServiceAuthorization() {
+        let authorizationStatus: CLAuthorizationStatus
+        
+        if #available(iOS 14.0, *) {
+            authorizationStatus = locationManager.authorizationStatus
+        } else {
+            authorizationStatus = CLLocationManager.authorizationStatus()
+        }
+        
+        if CLLocationManager.locationServicesEnabled() {
+            checkUserCurrentLocationAuthorization(authorizationStatus)
+        } else {
+            print("위치 서비스가 꺼져 있어 위치 권한 요청을 하지 못합니다.")
+        }
+    }
+    
+    func checkUserCurrentLocationAuthorization(_ authorizationStatus: CLAuthorizationStatus) {
+        switch authorizationStatus {
+        case .notDetermined:
+            print("NOT DETERMINED")
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+        case .restricted, .denied:
+            print("DENIED, 아이폰 설정으로 유도")
+            showRequestLocationServiceAlert()
+        case .authorizedWhenInUse:
+            print("WHEN IN USE")
+            locationManager.startUpdatingLocation()
+        default:
+            print("DEFAULT")
+        }
+    }
+    
+    func showRequestLocationServiceAlert() {
+        let requestLocationServiceAlert = UIAlertController(title: "위치정보 이용", message: "위치 서비스를 사용할 수 없습니다. 기기의 '설정 > 개인정보 보호'에서 위치 서비스를 켜주세요.", preferredStyle: .alert)
+        
+        let goSetting = UIAlertAction(title: "설정으로 이동", style: .destructive) { _ in
+            if let appSetting = URL(string: UIApplication.openSettingsURLString) {
+                UIApplication.shared.open(appSetting)
+            }
+        }
+        
+        let cancel = UIAlertAction(title: "취소", style: .default)
+        requestLocationServiceAlert.addAction(cancel)
+        requestLocationServiceAlert.addAction(goSetting)
+        
+        present(requestLocationServiceAlert, animated: true, completion: nil)
+    }
+}
+
+// MARK: - CLLocation Protocol
+
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(#function, locations)
+        
+        if let coordinate = locations.last?.coordinate {
+            setRegionAndAnnotation(center: coordinate, title: "나의 현재 위치")
+        }
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(#function, error)
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        print(#function)
+        checkUserDeviceLocationServiceAuthorization()
+    }
+}
+
+
