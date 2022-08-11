a80311f4dafb879e5d34eea85f20af78aeb81913
so yeon <alicesysy@swu.ac.kr>
2022-08-11 11:56:02 +0900

    [#30] MapView 구현

---
+++ Week6/Week6/Screen/Main/Controller/MapViewController.swift
@@ -0,0 +1,71 @@
+//
+//  MapViewController.swift
+//  Week6
+//
+//  Created by 소연 on 2022/08/11.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+/*
+ MapView
+ - 지도와 위치 권한은 상관 X
+ - 만약 지도에 현재 위치 등을 표현하고 싶다면 위치 권한을 등록해야 함
+ - 중심, 범위 지정
+ - 핀 (별도 구현) -> 명칭 : 어노테이션
+ */
+
+/*
+ 권한 : 반영이 느릴 수 있음 지웠다가 실행한다고 하더라도 > 한번 허용 
+ */
+
+final class MapViewController: UIViewController {
+
+    // MARK: - UI Property
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    // MARK: - Property
+    
+    let locationManager = CLLocationManager()
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setRegionAndAnnotation()
+    }
+
+    func setRegionAndAnnotation() {
+        // 지도 중심 설정 : 애플맵 활용해 좌표 복사
+        // 위도, 경도에 대한 정보
+        // - 사용자의 위치 정보를 모르더라도 사용 가능한 정보
+        let center = CLLocationCoordinate2D(latitude: 37.517829, longitude: 126.886270)
+
+        // 지도 중심 기반으로 보여질 범위 설정
+        let region = MKCoordinateRegion(center: center, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        mapView.setRegion(region, animated: true)
+
+        // 지도에 핀 추가
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = center
+        annotation.title = "이곳이 나의 캠퍼스다"
+        mapView.addAnnotation(annotation)
+    }
+}
+
+extension MapViewController: MKMapViewDelegate {
+
+    // 지도에 커스텀 핀 추가
+//    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+//
+//    }
+    
+    // ex. Kakao T - 탑승 위치 수정 시 택시 위치 변경되는 것
+//    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+//        
+//    }
+}
