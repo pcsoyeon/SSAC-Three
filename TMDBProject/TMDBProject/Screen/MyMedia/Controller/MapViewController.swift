7efb1eb0de884cf099c0da8a6a9dae0103eb4450
so yeon <alicesysy@swu.ac.kr>
2022-08-11 23:12:21 +0900

    [#30] MapView 관련 파일 추가

---
+++ TMDBProject/TMDBProject/Screen/MyMedia/Controller/MapViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MapViewController.swift
+//  TMDBProject
+//
+//  Created by 소연 on 2022/08/11.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    
+
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureMapView()
+    }
+    
+    // MARK: - Custom Method
+    
+    private func configureMapView() {
+        
+    }
+}
---
+++ TMDBProject/TMDBProject/Screen/MyMedia/Model/TheaterDataModel.swift
@@ -0,0 +1,8 @@
+//
+//  TheaterDataModel.swift
+//  TMDBProject
+//
+//  Created by 소연 on 2022/08/11.
+//
+
+import Foundation
