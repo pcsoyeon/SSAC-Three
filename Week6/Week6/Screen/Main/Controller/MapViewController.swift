68cbdb7ba377a9a01ded64cf08ac8cdf983a04e5
so yeon <alicesysy@swu.ac.kr>
2022-08-12 09:28:59 +0900

    [#30] 사용자 위치로 지도 이동

--- Week6/Week6/Screen/Main/Controller/MapViewController.swift
+++ Week6/Week6/Screen/Main/Controller/MapViewController.swift
@@ -19,7 +19,7 @@ import CoreLocation  // Location1. import
 
 /*
  권한: 반영이 조금씩 느릴 수 있음. 지웠다가 실행한다고 하더라도, 한 번 허용
- 설정: 앱이 바로 안 뜰 수 있음 
+ 설정: 앱이 바로 안 뜰 수 있음
  */
 
 final class MapViewController: UIViewController {
@@ -27,7 +27,6 @@ final class MapViewController: UIViewController {
     // MARK: - UI Property
     
     @IBOutlet weak var mapView: MKMapView!
-    
     
     // Location2. 위치에 대한 대부분을 담당
     let locationManager = CLLocationManager()
@@ -38,15 +37,15 @@ final class MapViewController: UIViewController {
         // Location3. 프로토콜 연결
         locationManager.delegate = self
         
-        checkUserDeviceLocationServiceAuthorization()
-        setRegionAndAnnotation()
+//        checkUserDeviceLocationServiceAuthorization() - 제거 가능한 이유 알기
+        let center = CLLocationCoordinate2D(latitude: 37.517829, longitude: 126.886270)
+        setRegionAndAnnotation(center: center)
     }
     
-    func setRegionAndAnnotation() {
+    func setRegionAndAnnotation(center: CLLocationCoordinate2D) {
         // 지도 중심 설정 : 애플맵 활용해 좌표 복사
         // 위도, 경도에 대한 정보
         // - 사용자의 위치 정보를 모르더라도 사용 가능한 정보
-        let center = CLLocationCoordinate2D(latitude: 37.517829, longitude: 126.886270)
         
         // 지도 중심 기반으로 보여질 범위 설정
         let region = MKCoordinateRegion(center: center, latitudinalMeters: 1000, longitudinalMeters: 1000)
@@ -104,6 +103,7 @@ extension MapViewController {
             
         case .restricted, .denied:
             print("DENIED, 아이폰 설정으로 유도")
+            
         case .authorizedWhenInUse:
             print("WHEN IN USE")
             // 사용자가 위치를 혀용해 둔 상태라면, startUpdatingLocation을 통해 didUpdateLocations 메서드가 실행
@@ -111,6 +111,25 @@ extension MapViewController {
         default:
             print("DEFAULT")
         }
+    }
+    
+    /*
+     Location Authorization Custom Alert
+     */
+    // 설정까지 이동 || 설정 세부 화면까지 이동
+    // 한번도 설정 앱에 들어가지 않았거나 막 다운 받은 앱이거나
+    func showRequestLocationServiceAlert() {
+      let requestLocationServiceAlert = UIAlertController(title: "위치정보 이용", message: "위치 서비스를 사용할 수 없습니다. 기기의 '설정>개인정보 보호'에서 위치 서비스를 켜주세요.", preferredStyle: .alert)
+      let goSetting = UIAlertAction(title: "설정으로 이동", style: .destructive) { _ in
+          if let appSetting = URL(string: UIApplication.openSettingsURLString) {
+              UIApplication.shared.open(appSetting)
+          }
+      }
+      let cancel = UIAlertAction(title: "취소", style: .default)
+      requestLocationServiceAlert.addAction(cancel)
+      requestLocationServiceAlert.addAction(goSetting)
+      
+      present(requestLocationServiceAlert, animated: true, completion: nil)
     }
 }
 
@@ -120,11 +139,35 @@ extension MapViewController: CLLocationManagerDelegate {
     // Location5. 사용자의 위치를 성공적으로 가지고 온 경우
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         print(#function, locations)
+        
+        // ex) 위도, 경도 기반으로 날씨 정보 조회
+        // ex) 지도를 다시 세팅
+        // 현재 사용자의 위치로 이동, title은 변하지 않음
+        if let coordinate = locations.last?.coordinate {
+            setRegionAndAnnotation(center: coordinate)
+        }
+        
+        // 적당한 시점에 위치 업데이트 중지 
+        locationManager.stopUpdatingLocation() // 어느정도 위치가 바뀌면 > accuarcy가 호출되어서 위치 변경 감지
     }
     
     // Location6. 사용자의 위치를 못 가지고 온 경우
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print(#function, error)
+    }
+    
+    // Location9. 사용자의 권한 상태가 바뀔 때를 알려줌
+    // 거부 했다가 설정에서 변경 || notDetermined에서 허용을 했거나 등
+    // 허용 했어서 위치를 가지고 오는 와중에 설정에서 거부하고 돌아온다면?
+    // iOS 14 이상: 사용자의 권한 상태가 변경될 때, 위치 관리자 생성할 때 호출됨
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        print(#function)
+        checkUserDeviceLocationServiceAuthorization()
+    }
+    
+    // iOS 14 미만
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
     }
 }
 
