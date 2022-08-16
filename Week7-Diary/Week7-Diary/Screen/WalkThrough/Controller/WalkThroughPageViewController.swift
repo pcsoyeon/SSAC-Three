ee71a17b1990299895247f4e42c468e2de57cf9b
so yeon <alicesysy@swu.ac.kr>
2022-08-16 12:07:29 +0900

    [#33] UIPageViewController 추가

---
+++ Week7-Diary/Week7-Diary/Screen/WalkThrough/Controller/WalkThroughPageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WalkThroughPageViewController.swift
+//  Week7-Diary
+//
+//  Created by 소연 on 2022/08/16.
+//
+
+import UIKit
+
+class WalkThroughPageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
