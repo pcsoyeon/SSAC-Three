8780c75eff7bc54f4b6ef89bb74fe30ad071079c
so yeon <alicesysy@swu.ac.kr>
2022-08-16 11:28:06 +0900

    [#33] filePrivate / private 비교

--- Week6/Week6/Network/APIManager/KakaoAPIManager.swift
+++ Week6/Week6/Network/APIManager/KakaoAPIManager.swift
@@ -10,6 +10,25 @@ import Foundation
 import Alamofire
 import SwiftyJSON
 
+struct User {
+    fileprivate let name = "고래밥" // 같은 스위프트 파일에서 다른 클래스, 구조체에서 사용 가능 / 다른 스위프트 파일에서는 접근 불가
+    private let age = 11 // 같은 스위프트 파일 내에서 같은 파일
+}
+
+extension User {
+    func example() {
+        print(self.name, self.age)
+    }
+}
+
+struct Person {
+    func example() {
+        let user = User()
+        user.name // Ok
+//        user.age // error
+    }
+}
+
 class KakaoAPIManager {
     static let shared = KakaoAPIManager()
     
