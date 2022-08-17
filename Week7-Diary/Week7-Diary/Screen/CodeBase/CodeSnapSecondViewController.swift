02071d34c86ee9aa742c2d761ff99ec850773b98
so yeon <alicesysy@swu.ac.kr>
2022-08-17 11:58:52 +0900

    [#34] 스냅킷으로 UI/레이아웃 구현

--- Week7-Diary/Week7-Diary/Screen/CodeBase/CodeSnapSecondViewController.swift
+++ Week7-Diary/Week7-Diary/Screen/CodeBase/CodeSnapSecondViewController.swift
@@ -10,8 +10,39 @@ import UIKit
 import SnapKit
 
 class CodeSnapSecondViewController: UIViewController {
+    
+    // MARK: - UI Property
+    
+    let blackView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        return view
+    }()
+    
+    let redView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        return view
+    }()
 
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 코드 순서 > 뒤에 있는 것이 위에 존재
+        [redView, blackView].forEach {
+            view.addSubview($0)
+        }
+        
+        redView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.width.height.equalTo(200)
+            make.centerX.equalTo(view)
+        }
+        
+        blackView.snp.makeConstraints { make in
+            make.edges.equalTo(redView).inset(50)
+        }
     }
 }
--- Week7-Diary/Week7-Diary/Screen/Main/ViewController.swift
+++ Week7-Diary/Week7-Diary/Screen/Main/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
 //        showActivityViewController()
 //        OpenWebView.presentWebViewController(self, url: "https://www.naver.com", transitionStyle: .present)
         
-        let viewController = CodeSnapViewController()
+        let viewController = CodeSnapSecondViewController()
         viewController.modalPresentationStyle = .overFullScreen
         present(viewController, animated: true)
     }
