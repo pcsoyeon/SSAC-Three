4c151d3354548802639fc3524a34023f1cc5d430
so yeon <alicesysy@swu.ac.kr>
2022-08-18 11:32:46 +0900

    [#21] Swift 문법 - Generic

--- TypeCastingGeneric.playground/Contents.swift
+++ TypeCastingGeneric.playground/Contents.swift
@@ -14,11 +14,11 @@ type(of: value)
 // ex. 애플 구글 모바일
 class Mobile {
     let name: String
-
+    
     var introduce: String {
         return "\(name) 입니다."
     }
-
+    
     init(name: String) {
         self.name = name
     }
@@ -27,7 +27,7 @@ class Mobile {
 // Google, Apple이 부모 클래스 Mobile을 상속받고 있음
 // 부모의 이니셜라이저를 사용할 수 있음
 class Google: Mobile {
-
+    
 }
 
 class Apple: Mobile {
@@ -93,9 +93,118 @@ print(somethings[1])
 let example = somethings[1]
 
 if var element = example as? String {
-//    print(element.toggle())
+    //    print(element.toggle())
     print(element)
 } else {
     print("Bool 아님")
 }
 
+// MARK: - Generic
+
+/*
+ Generic
+ - 타입에 유연하게 대응
+ - Type Parameter: 플레이스 홀더 같은 역할, 어떤 타입인지 타입의 종류는 알려주지 않음, 특정한 타입 하나라는 건 알 수 있음, 제너릭으로 이루어진 함수 사용 시 T에 들어갈 타입은 모두 같아야 한다.
+ - Type Constraints: 클래스/프로토콜 제약
+ */
+
+func configureBorderLabel(_ view: UILabel) {
+    view.backgroundColor = .clear
+    view.clipsToBounds = true
+    view.layer.cornerRadius = 8
+    view.layer.borderColor = UIColor.black.cgColor
+    view.layer.borderWidth = 1
+}
+
+func configureBorderButton(_ view: UIButton) {
+    view.backgroundColor = .clear
+    view.clipsToBounds = true
+    view.layer.cornerRadius = 8
+    view.layer.borderColor = UIColor.black.cgColor
+    view.layer.borderWidth = 1
+}
+
+func configureBorderTextField(_ view: UITextField) {
+    view.backgroundColor = .clear
+    view.clipsToBounds = true
+    view.layer.cornerRadius = 8
+    view.layer.borderColor = UIColor.black.cgColor
+    view.layer.borderWidth = 1
+}
+
+// SOKYTE 라는 타입 -> UIView를 상속받는 객체들
+// UIView 라는 타입만 충족시키면 매개변수로 들어올 수 있다.
+
+func configureBorder<T: UIView>(_ view: T) {
+    view.backgroundColor = .clear
+    view.clipsToBounds = true
+    view.layer.cornerRadius = 8
+    view.layer.borderColor = UIColor.black.cgColor
+    view.layer.borderWidth = 1
+}
+
+let image = UIImageView()
+let label = UILabel()
+
+configureBorder(image)
+configureBorder(label)
+
+struct DummyData<T, U> {
+//    var name: T
+    var mainContents: T
+    var subContents: U
+}
+
+//let data = DummyData(name: "abc")
+//let intData = DummyData(name: 500)
+
+let cast = DummyData(mainContents: "현빈", subContents: "주인공역")
+let phoneNumber = DummyData(mainContents: "고래밥", subContents: "010-1234-5678")
+let todo = DummyData(mainContents: "", subContents: true)
+
+func total(a: [Int]) -> Int {
+    return a.reduce(0, +)
+}
+total(a: [1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+func total<T: Numeric>(a: [T]) -> T {
+    return a.reduce(.zero, +)
+}
+
+// 화면 전환 코드
+class SampleViewController: UIViewController {
+    func transitionViewController<T: UIViewController>(sb: String, id: T) {
+        let sb = UIStoryboard(name: sb, bundle: nil)
+        let vc = sb.instantiateViewController(identifier: String(describing: id)) as! T
+        
+        self.present(vc, animated: true)
+    }
+}
+
+class Animal: Equatable {
+    static func == (lhs: Animal, rhs: Animal) -> Bool {
+        return lhs.name == rhs.name ? true : false
+    }
+    
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+let one = Animal(name: "고양이")
+let two = Animal(name: "고양이")
+
+one == two
+
+var fruit1 = "사과"
+var fruit2 = "바나나"
+swap(&fruit1, &fruit2)
+print(fruit1, fruit2)
+
+func swapTwoInts<T>(a: inout T, b: inout T) {
+    let tempA = a
+    a = b
+    b = tempA
+}
