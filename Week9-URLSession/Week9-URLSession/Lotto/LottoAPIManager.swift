12567401d1d69f720b7bb59224f4ccae2b4a1ae4
so yeon <alicesysy@swu.ac.kr>
2022-08-30 11:02:47 +0900

    [#41] 서버 통신 후 UI 업데이트

--- Week9-URLSession/Week9-URLSession/Lotto/LottoAPIManager.swift
+++ Week9-URLSession/Week9-URLSession/Lotto/LottoAPIManager.swift
@@ -12,42 +12,54 @@ import Foundation
 // default configuration - shared 설정 유사, 커스텀 가능, 응답 클로젖 + 델리게이트
 // 무엇이 커스텀 가능? 셀룰러 연결/타임아웃 간격
 
+enum APIError: Error {
+    case invalidResponse
+    case noData
+    case failedRequest
+    case invalidData
+}
+
 class LottoAPIManager {
     
-    static func requestLotto(drwNo: Int) {
+    static func requestLotto(drwNo: Int, completionHandler: @escaping (Lotto?, APIError?) -> ()) {
         guard let url = URL(string: "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=\(drwNo)") else { return }
         
         URLSession.shared.dataTask(with: url) { data, response, error in
-            guard error == nil else {
-                print("Failed Request")
-                return
-            }
             
-            guard let data = data else {
-                print("No Data Returned")
-                return
-            }
-            
-            guard let response = response as? HTTPURLResponse else {
-                print("Unable Response")
-                return
-            }
-            
-            guard response.statusCode == 200 else {
-                print("Failed Response")
-                return
-            }
-            
-            do {
-                let result = try JSONDecoder().decode(Lotto.self, from: data)
-                print(result)
+            DispatchQueue.main.async {
+                guard error == nil else {
+                    print("Failed Request")
+                    completionHandler(nil, .failedRequest)
+                    return
+                }
                 
-                print(result.drwNoDate)
+                guard let data = data else {
+                    print("No Data Returned")
+                    completionHandler(nil, .noData)
+                    return
+                }
                 
-            } catch let error {
-                print(error)
+                guard let response = response as? HTTPURLResponse else {
+                    print("Unable Response")
+                    completionHandler(nil, .invalidResponse)
+                    return
+                }
+                
+                guard response.statusCode == 200 else {
+                    print("Failed Response")
+                    completionHandler(nil, .failedRequest)
+                    return
+                }
+                
+                do {
+                    let result = try JSONDecoder().decode(Lotto.self, from: data)
+                    completionHandler(result, nil)
+                    
+                } catch let error {
+                    completionHandler(nil, .invalidData)
+                    print(error)
+                }
             }
-            
             
         }.resume()
     }
--- Week9-URLSession/Week9-URLSession/ViewController.swift
+++ Week9-URLSession/Week9-URLSession/ViewController.swift
@@ -8,13 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var label: UILabel = {
+        let label = UILabel()
+        label.textColor = .darkGray
+        label.font = .systemFont(ofSize: 15)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        LottoAPIManager.requestLotto(drwNo: 1011)
+        setConstraints()
+        callRequest()
     }
-
-
+    
+    private func setConstraints() {
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+    }
 }
 
+// MARK: - Network
+
+extension ViewController {
+    private func callRequest() {
+        LottoAPIManager.requestLotto(drwNo: 1011) { lotto, error in
+            guard let lotto = lotto else {
+                return
+            }
+            
+            self.label.text = "\(lotto.drwNoDate)"
+        }
+    }
+}
