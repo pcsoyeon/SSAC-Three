e75e846de82fcb9532854f654c615aaad65af6ab
so yeon <alicesysy@swu.ac.kr>
2022-10-11 08:45:59 +0900

    [#45] firebase 연동 구현

--- FirebaseExample/FirebaseExample/ViewController.swift
+++ FirebaseExample/FirebaseExample/ViewController.swift
@@ -7,12 +7,30 @@
 
 import UIKit
 
+import FirebaseAnalytics
+import SYKit
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 커스텀
+        Analytics.logEvent("rejack", parameters: [
+          "name": "소깡", // user name (UserDefaults, Realm ..)
+          "full_text": "안녕하세요?",
+        ])
+        
+        // 기본 
+        Analytics.setDefaultEventParameters([
+          "level_name": "Caverns01",
+          "level_difficulty": 4
+        ])
     }
 
 
+    @IBAction func touchUpCrashButton(_ sender: UIButton) {
+        
+    }
 }
 
