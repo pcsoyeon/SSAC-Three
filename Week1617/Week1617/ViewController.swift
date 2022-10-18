a54947c79f1cc2988b617b355fe9cdd90f0a2e6e
so yeon <alicesysy@swu.ac.kr>
2022-10-18 09:44:37 +0900

    [#50] 16,17 주차 프로젝트 추가

---
+++ Week1617/Week1617/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week1617
+//
+//  Created by 소연 on 2022/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
