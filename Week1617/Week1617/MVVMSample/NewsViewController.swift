1965c181c2fc079d0b8d361ae27124ebc48e0f1e
so yeon <alicesysy@swu.ac.kr>
2022-10-20 10:58:46 +0900

    [#52] Diffable, Compositional으로 UI 구현

--- Week1617/Week1617/MVVMSample/NewsViewController.swift
+++ Week1617/Week1617/MVVMSample/NewsViewController.swift
@@ -12,15 +12,21 @@ class NewsViewController: UIViewController {
     // MARK: - UI Property
     
     @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var collectionView: UICollectionView!
     
     // MARK: - Property
     
     private var viewModel = NewsViewModel()
     
+    var dataSource: UICollectionViewDiffableDataSource<Int, News.NewsItem>!
+    
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureHierachy()
+        configureDataSource()
         
         viewModel.pageNumber.bind { value in
             print("bind == \(value)")
@@ -37,5 +43,42 @@ class NewsViewController: UIViewController {
         if let text = numberTextField.text {
             viewModel.changePageNumberFormat(text: text)
         }
+    }
+}
+
+extension NewsViewController {
+    func configureHierachy() { // addSubView, init, snapkit
+        collectionView.collectionViewLayout = createLayout()
+        collectionView.backgroundColor = .systemPink
+    }
+    
+    func configureDataSource() {
+        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, News.NewsItem>.init { cell, indexPath, itemIdentifier in
+            
+            var content = UIListContentConfiguration.valueCell()
+            
+            content.text = itemIdentifier.title
+            content.secondaryText = itemIdentifier.body
+            
+            cell.contentConfiguration = content
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            
+            let cell = collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+            return cell
+        })
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Int, News.NewsItem>()
+        snapshot.appendSections([0])
+        snapshot.appendItems(News.items)
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+    
+    func createLayout() -> UICollectionViewLayout {
+        let configuration = UICollectionLayoutListConfiguration(appearance: .insetGrouped
+        )
+        let layout = UICollectionViewCompositionalLayout.list(using: configuration)
+        return layout
     }
 }
