106a0f2779268454763b679d14a8c72d813ca727
so yeon <alicesysy@swu.ac.kr>
2022-11-02 14:36:53 +0900

    [#59] 회원가입, 로그인, 프로필 서버 연결

---
+++ Week18/Week18/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Week18
+//
+//  Created by 소연 on 2022/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let service = APIService()
+//        service.login()
+        service.profile()
+    }
+
+
+}
+
